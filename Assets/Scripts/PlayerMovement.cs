89b49381858d19d20d4883cf49da4160ad7ba1b4
OnurKalkan <onurkalkanv84@hotmail.com>
2023-10-24 11:53:10 +0300

    Movement A-D

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 1f;
+    float speed = 0.05f;
+    public GameObject warrior;
+    bool tapToStart = false;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(0,0,1) * speed);
+        if (tapToStart)
+            transform.Translate(Vector3.forward * speed);//movement code
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("enter pressed");//shows a enter pressed message on console when space pressed
+            //transform.Find("Warrior").GetComponent<Animator>().SetBool("RunStart", true);//search the warrior in children
+            //GameObject.Find("Warrior").GetComponent<Animator>().SetBool("RunStart", true);//search the warrior in game scene
+            warrior.GetComponent<Animator>().SetBool("RunStart", true);//reference to the warrior with a game object variable
+            tapToStart = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))//if I press A it will slide to left
+        {
+            if(warrior.transform.localPosition.x > -2)
+                warrior.transform.localPosition -= new Vector3(2,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))//if I press D it will slide to right
+        {
+            if (warrior.transform.localPosition.x < 2)
+                warrior.transform.localPosition += new Vector3(2, 0, 0);
+        }
     }
 }
